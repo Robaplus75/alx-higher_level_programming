3b9fafea0e218214f8cb5c74f84e7de58403d11c
Robel <robelwon75@gmail.com>
2023-06-14 16:58:58 +0300

    task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,10 +3,9 @@
 
 def search_replace(my_list, search, replace):
     newlist = []
-    for i in range(len(my_list))
+    for i in range(len(my_list)):
         if my_list[i] == search:
             newlist.append(replace)
         else:
             newlist.append(my_list[i])
     return newlist
-
