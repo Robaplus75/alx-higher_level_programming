101b83a4f44b7bc953c79cc1cf85ca2def347011
Robel <robelwon75@gmail.com>
2023-07-08 21:34:35 +0300

    task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""Module for integers addition task"""
+
+def add_integer(a, b=98):
+
+    """ Adds two integers """
+
+    if type(a) == float:
+        a = int(a)
+    elif type(a) == int:
+        pass
+    else:
+        raise TypeError("a must be an integer")
+
+    if type(b) == float:
+        b = int(b)
+    elif type(b) == int:
+        pass
+    else:
+        raise TypeError("b must be an integer")
+    return a + b
