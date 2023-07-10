9b6070de5735200f46b9f68e0074e216a7d123ee
Robel <robelwon75@gmail.com>
2023-07-10 22:38:42 +0300

    task 101

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -6,5 +6,5 @@ def add_attribute(obj, att, value):
     """Adds new attributes to the object if it can """
     if not hasattr(obj, "__dict__"):
         raise TypeError("can't add new attribute")
-    #setattr function called
+    # setattr function called
     setattr(obj, att, value)
