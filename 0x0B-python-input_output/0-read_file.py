04cc568cf5569a385766107db8907e6faf31a002
Robel <robelwon75@gmail.com>
2023-07-11 12:32:50 +0300

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+""" defines the read_file function"""
 
 
 def read_file(filename=""):
-    with open(filename, 'r', encoding = 'utf-8') as myf:
+    """opens the file and prints it's content"""
+    with open(filename, 'r', encoding='utf-8') as myf:
         text = myf.read()
     print(text, end="")
