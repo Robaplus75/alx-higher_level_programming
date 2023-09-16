1e8cdb79596ab756810ab5993ac6df6a023a2672
robel <robelwon75@gmail.com>
2023-09-16 11:57:06 +0300

    task 0 fix8

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,7 @@ from sys import argv
 for listing all states in the database
 """
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', port=3306,
            user=argv[1], password=argv[2], db=argv[3])
     cur = db.cursor()
