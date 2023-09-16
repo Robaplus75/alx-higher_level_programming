70d7aa602c33f21e9e44026db359d4cd176a9efb
robel <robelwon75@gmail.com>
2023-09-16 14:55:54 +0300

    task 2 fix1

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,17 +4,20 @@ A script that takes in an argument and displays all values
 in states table where name matches the argument
 """
 
-import MySQLdb
-from sys import argv
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
 
-db_conn = MySQLdb.connect(
-        host='localhost', port=3306, user=argv[1], password=argv[2],
-        db=argv[3])
+    db_conn = MySQLdb.connect(
+            host='localhost', port=3306, user=argv[1], password=argv[2],
+            db=argv[3])
 
-cur = db_conn.cursor()
-cur.execute("SELECT * FROM states WHERE name='{}' ORDER BY id;".format(argv[4]))
+    cur = db_conn.cursor()
+    cur.execute(
+            "SELECT * FROM states WHERE name='{}'ORDER BY id;".format(
+                argv[4]))
 
-output = cur.fetchall()
+    output = cur.fetchall()
 
-for out in output:
-    print(out)
+    for out in output:
+        print(out)
