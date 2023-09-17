b45209c0a0e254afbab1b11e46b18869c816260d
robel <robelwon75@gmail.com>
2023-09-17 20:47:05 +0300

    task 7

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+script that lists all state object form the databse
+"""
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sys import argv
+from model_state import Base, State
+
+engine = create_engine(
+        f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}")
+
+Session = sessionmaker(engine)
+
+localSession = Session()
+
+mystates = localSession.query(State).order_by(State.id).all()
+
+for state in mystates:
+    print(f"{state.id}: {state.name}")
