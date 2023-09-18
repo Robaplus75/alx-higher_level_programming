40ac54fc89f64b11f18a35287458edf9bb2b8b14
robel <robelwon75@gmail.com>
2023-09-18 10:20:32 +0300

    task 9

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import State
+
+if __name__ == "__main__":
+    engine = create_engine(
+            f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}")
+    Session = sessionmaker(engine)
+    localSession = Session()
+
+    output = localSession.query(State).filter(State.name.like('%a%')).all()
+
+    for i in output:
+        print(f"{i.id}: {i.name}")
+    localSession.close()
